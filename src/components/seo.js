35da6ae9f87eb3a36303b6318c42ab95479286bd
trakbit <harsh.vardhan.611@gmail.com>
2020-10-21 15:15:55 +0530

    Update seo.js

--- src/components/seo.js
+++ src/components/seo.js
@@ -85,6 +85,7 @@ function SEO({ description, lang, meta, title, keywords }) {
 SEO.defaultProps = {
   lang: `en`,
   meta: [],
+  keywords: [],
   description: ``,
 }
 
@@ -93,6 +94,7 @@ SEO.propTypes = {
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
   title: PropTypes.string.isRequired,
+  keywords: PropTypes.arrayOf(PropTypes.string)
 }
 
 export default SEO
