f17bfbb736436a9a946b299026f19343dfd17316
trakbit <harsh.vardhan.611@gmail.com>
2020-10-26 22:52:01 +0530

    push fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import styled from "styled-components"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import '../../App.css'
+require('typeface-rubik')
 
 const FlexCol = styled.div`
   display: flex;
