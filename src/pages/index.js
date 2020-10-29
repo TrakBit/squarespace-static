ed4b154d3fbfb9306d5e12f3334fcddb9be9adb1
trakbit <harsh.vardhan.611@gmail.com>
2020-10-29 10:08:40 +0530

    picture tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,18 +72,34 @@ const IndexPage = () => {
       <Container style={{ backgroundColor: "#f4f9f4" }}>
         <Banner windowWidth={windowWidth} />
       </Container>
-      <Container>
-        <img
-          style={{
-            width: '90%',
-            borderImageWidth: '2px',
-            borderColor: '#00000',
-            paddingTop: '70px'
-          }}
-          alt="squarespace whatsapp"
-          src={"https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp.webp?alt=media&token=85316663-0cd5-48a2-b946-4732162fad79"}
-        />
-      </Container>
+
+
+      <div style={{ textAlign: 'center'}}>
+        <Container> 
+          <picture>
+            <source
+              style={{
+                width: '90%',
+                borderImageWidth: '2px',
+                borderColor: '#00000',
+                paddingTop: '70px'
+              }}
+              type="image/webp"
+              srcset={"https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp.webp?alt=media&token=85316663-0cd5-48a2-b946-4732162fad79"}
+            />
+            <img
+              style={{
+                width: '90%',
+                borderImageWidth: '2px',
+                borderColor: '#00000',
+                paddingTop: '70px'
+              }}
+              alt="squarespace whatsapp"
+              src={"https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp.png?alt=media&token=12475eb1-d5cd-400f-b996-12ecff1850ef"}
+            />
+          </picture>
+        </Container>
+      </div>
 
       <Container style={{ margin: '0 auto', width: screenWidth }}>
         <h1 style={{ fontWeight: "600", fontSize: '35px', textAlign: 'center', lineHeight: "1.3" }}>
@@ -136,11 +152,18 @@ const IndexPage = () => {
             href={"https://salesjump.xyz/blog/add-whatsapp-live-chat-squarespace"}
           >
             <div className="card">
-              <img
-                src={"https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp-design-widget.webp?alt=media&token=7cb516c8-8f0a-4460-868f-6438182671f6"}
-                alt="Avatar"
-                style={{ width: '100%' }}
-              />
+              <picture>
+                <source
+                  srcset={"https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp-design-widget.webp?alt=media&token=7cb516c8-8f0a-4460-868f-6438182671f6"}
+                  type="image/webp"
+                  style={{ width: '100%' }}
+                />
+                <img
+                  src={"https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp-design-widget.png?alt=media&token=d5dc7cf9-b667-4e00-9573-79f03b80f567"}
+                  alt="Avatar"
+                  style={{ width: '100%' }}
+                />
+              </picture>
               <Container>
                 <h4>
                   <b>
@@ -169,11 +192,22 @@ const IndexPage = () => {
               href={"https://salesjump.xyz/blog/whatsapp-chat-squarespace"}
             >
               <div className="card">
-                <img
-                  src={"https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp.webp?alt=media&token=85316663-0cd5-48a2-b946-4732162fad79"}
-                  alt="Avatar"
-                  style={{ width: '100%' }}
-                />
+                <picture>
+                  <source
+                    style={{
+                      width: '100%'
+                    }}
+                    type="image/webp"
+                    srcset={"https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp.webp?alt=media&token=85316663-0cd5-48a2-b946-4732162fad79"}
+                  />
+                  <img
+                    style={{
+                      width: '100%'
+                    }}
+                    alt="squarespace whatsapp"
+                    src={"https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp.png?alt=media&token=12475eb1-d5cd-400f-b996-12ecff1850ef"}
+                  />
+                </picture>
                 <Container>
                   <h4>
                     <b>
@@ -193,11 +227,22 @@ const IndexPage = () => {
               href={"https://salesjump.xyz/blog/squarespace-help-chat-live"}
             >
               <div className="card">
-                <img
-                  src={"https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp-widget.webp?alt=media&token=e536f4b6-1ecc-4ef7-bd26-39bc08af2186"}
-                  alt="Avatar"
-                  style={{ width: '100%' }}
-                />
+                <picture>
+                  <source
+                    style={{
+                      width: '100%'
+                    }}
+                    type="image/webp"
+                    srcset={"https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp-widget.webp?alt=media&token=e536f4b6-1ecc-4ef7-bd26-39bc08af2186"}
+                  />
+                  <img
+                    style={{
+                      width: '100%'
+                    }}
+                    alt="squarespace whatsapp"
+                    src={"https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp-widget.jpeg?alt=media&token=3b849960-7dde-4ab7-81a3-27770d0e4ec4"}
+                  />
+                </picture>
                 <Container>
                   <h4>
                     <b>
@@ -217,34 +262,6 @@ const IndexPage = () => {
 }
 
 const Banner = ({ windowWidth }) => {
-  if (windowWidth > 480) {
-    return (
-      <>
-        <h1 style={{ fontSize: '5vw', lineHeight: "1.5", fontWeight: "600" }}>
-          📞 WhatsApp Chat on your
-                  <br />Squarespace online store. 🛍️
-        </h1>
-        <h1 style={{ fontSize: '2vw', lineHeight: "1.5", fontWeight: "600", marginTop: "20px" }}>
-          Add the human touch to your store
-        </h1>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <h1 style={{ fontSize: '9vw', textAlign: 'center', lineHeight: "1.3" }}>
-          📞 WhatsApp Chat on your
-                  <br />Squarespace online store. 🛍️
-        </h1>
-        <h1 style={{ fontSize: '4vw', lineHeight: "1.3" }}>
-          Add the human touch to your store
-        </h1>
-      </>
-    );
-  }
-}
-
-const Guide = ({ windowWidth }) => {
   if (windowWidth > 480) {
     return (
       <>
