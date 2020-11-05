b035be457ce21453667cd82c397a00fa47d9503d
trakbit <harsh.vardhan.611@gmail.com>
2020-11-05 16:34:35 +0530

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -23,13 +23,13 @@ const Blog = () => {
         <Layout >
             <SEO
                 title={'SalesJump - Add WhatsApp Chat To Squarespace'}
-                description={'Integrate WhatsApp Live Chat Widget With Squarespace'}
+                description={'Blogs on improving Squarespace customer support by integrating live chat'}
                 keywords={['Squarespace', 'WhatsApp']}
             />
             <div>
                 <br/>
-                <h1 style={{fontWeight: '600', fontSize: '35px', textAlign: 'center', lineHeight: '1.3'}}>
-                    Read more on why you need WhatsApp support for Squarespace
+                <h1 style={{textAlign: 'center', lineHeight: '1.3'}}>
+                    Blogs on Support, Service and Retention
                 </h1>
                 <div className='rcards'>
                     <div className='rcard'>
@@ -102,71 +102,71 @@ const Blog = () => {
             </div>
 
             <div style={{marginBottom: '5%'}}>
-                <h1 style={{fontWeight: '600', fontSize: '35px', textAlign: 'center', lineHeight: '1.3'}}>
-                    Add live chat support to Squarespace. But why?
-                </h1>
-                <Container>
-                    <Link
-                        style={{textDecoration: 'none', color: '#000000', minWidth: '140px', maxWidth: '500px'}}
-                        to={'/blog/squarespace-store-live-chat-support'}
-                    >
-                        <div className='card'>
-                            <picture>
-                                <source
-                                    style={{
-                                        width: '100%'
-                                    }}
-                                    type='image/avif'
-                                    srcSet={'https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp-support.avif?alt=media&token=c2304f5e-8500-4f4b-96a5-bd61ab71135c'}
-                                />
-                                <img
-                                    style={{
-                                        width: '100%'
-                                    }}
-                                    alt='squarespace-whatsapp'
-                                    src={'https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp-support.jpg?alt=media&token=90c525ce-0675-432a-b722-d9b8c8fd3a69'}
-                                />
-                            </picture>
-                            <Container>
-                                <h2 style={{fontWeight: '500', fontSize: '20px'}}>
-                                    Integrating live chat support to your Squarespace isn't just about support
-                                </h2>
-                            </Container>
-                        </div>
-                    </Link>
-                </Container>
-            </div>
 
-            <div style={{marginBottom: '5%'}}>
-                <h1 style={{fontWeight: '600', fontSize: '35px', textAlign: 'center', lineHeight: '1.3'}}>
-                    Guide to SEO your Squarespace store for FREE 🙂
-                </h1>
-                <Container>
-                    <Link
-                        style={{textDecoration: 'none', color: '#000000', minWidth: '140px', maxWidth: '500px'}}
-                        to={'/blog/squarespace-seo-guide'}
-                    >
-                        <div className='card'>
-                            <picture>
-                                <source
-                                    srcSet={'https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fdesktop-score.avif?alt=media&token=dbeabdb1-2f88-4936-9078-a6ac2e934183'}
-                                    type='image/avif'
-                                    style={{width: '100%'}}
-                                />
-                                <img
-                                    src={'https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fdesktop-score.png?alt=media&token=415e759a-189a-4d2a-9504-066ed9b4fde3'}
-                                    alt='squarespace-whatsapp-chat'
-                                    style={{width: '100%'}}
-                                />
-                            </picture>
-                            <Container>
-                                <h2 style={{fontWeight: '500', fontSize: '20px'}}>
-                                    How to SEO your Squarespace site [2020]: Guide
-                                </h2>
-                            </Container>
-                        </div>
-                    </Link>
-                </Container>
+                <div className='rcards'>
+                    <div className='rcard'>
+                        <Container>
+                            <Link
+                                style={{textDecoration: 'none', color: '#000000', minWidth: '140px', maxWidth: '500px'}}
+                                to={'/blog/squarespace-store-live-chat-support'}
+                            >
+                                <div className='card'>
+                                    <picture>
+                                        <source
+                                            style={{
+                                                width: '100%'
+                                            }}
+                                            type='image/avif'
+                                            srcSet={'https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp-support.avif?alt=media&token=c2304f5e-8500-4f4b-96a5-bd61ab71135c'}
+                                        />
+                                        <img
+                                            style={{
+                                                width: '100%'
+                                            }}
+                                            alt='squarespace-whatsapp'
+                                            src={'https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fsquarespace-whatsapp-support.jpg?alt=media&token=90c525ce-0675-432a-b722-d9b8c8fd3a69'}
+                                        />
+                                    </picture>
+                                    <Container>
+                                        <h2 style={{fontWeight: '500', fontSize: '20px'}}>
+                                            Integrating live chat support to your Squarespace isn't just about support
+                                        </h2>
+                                    </Container>
+                                </div>
+                            </Link>
+                        </Container>
+                    </div>
+
+                    <div className='rcard'>
+                        <Container>
+                            <Link
+                                style={{textDecoration: 'none', color: '#000000', minWidth: '140px', maxWidth: '500px'}}
+                                to={'/blog/squarespace-seo-guide'}
+                            >
+                                <div className='card'>
+                                    <picture>
+                                        <source
+                                            srcSet={'https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fdesktop-score.avif?alt=media&token=dbeabdb1-2f88-4936-9078-a6ac2e934183'}
+                                            type='image/avif'
+                                            style={{width: '100%'}}
+                                        />
+                                        <img
+                                            src={'https://firebasestorage.googleapis.com/v0/b/squarespace-chat.appspot.com/o/images%2Fdesktop-score.png?alt=media&token=415e759a-189a-4d2a-9504-066ed9b4fde3'}
+                                            alt='squarespace-whatsapp-chat'
+                                            style={{width: '100%'}}
+                                        />
+                                    </picture>
+                                    <Container>
+                                        <h2 style={{fontWeight: '500', fontSize: '20px'}}>
+                                            How to SEO your Squarespace site [2020]: Guide
+                                        </h2>
+                                    </Container>
+                                </div>
+                            </Link>
+                        </Container>
+                    </div>
+                </div>
+
             </div>
         </Layout>
     );
